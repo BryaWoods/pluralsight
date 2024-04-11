2fee59fadc8a2c8815fe0947d45b6ce41d062f89
BryaWoods <bryannewoods@gmail.com>
2024-04-11 15:31:51 -0400

    completed Mod3 excercise 2 - MathApp

--- java-development/workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ java-development/workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 public class MathApp {
     public static void main(String[] args) {
        // Question 1//
+
         double bobSalary = 50250;
         double garySalary = 28280;
 
@@ -25,21 +26,21 @@ public class MathApp {
         double area = Math.PI * Math.pow(radius, 2);
         System.out.println("The area of the circle is " + area);
 
-        //Question 3//
+        //Question 4//
 
         double num1 = 5.0;
         double sqrtNum1 = Math.sqrt(num1);
         System.out.println("The square root of " + num1 + "is " + sqrtNum1);
 
-        //Question 4//
-
         //Question 5//
+
+        //Question 6//
 
         double num2 = -3.8;
         double absNum2 = Math.abs(num2);
         System.out.println("The absolute value of " + num2 + "is" + absNum2);
 
-        //Question 6//
+        //Question 7//
 
         double randomNum = Math.random();
         System.out.println("The random number is " + randomNum);
