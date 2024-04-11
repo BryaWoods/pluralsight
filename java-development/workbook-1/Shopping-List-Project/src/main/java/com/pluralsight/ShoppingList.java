e8d630ba9eb869ae9acddb35bb2706e3d4dd058c
BryaWoods <bryannewoods@gmail.com>
2024-04-11 13:55:18 -0400

    initial commit

---
+++ java-development/workbook-1/Shopping-List-Project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Shopping list ");
+        System.out.println( "1. Bleach " +
+                "2. Knives " +
+                "3. Duct Tape " +
+                "4. Gloves " +
+                "5. Trash Bags " +
+                "6. Rain Coat " +
+                "7. Saw " +
+                "8. Ski Mask " +
+                "9. Salt " +
+                "10. Pepper ");
+
+    }
+}
