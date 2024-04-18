509b047d595d69d38b246af7f3006b368b062d8e
BryaWoods <bryannewoods@gmail.com>
2024-04-18 12:47:08 -0400

    update cellphone

---
+++ java-development/workbook-2/LoopingExercises/src/main/java/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 5) {
+            System.out.println("I <3 JAVA");
+            sum += i;
+            i++;
+        }
+
+    }
+}
---
+++ java-development/workbook-2/LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        do {
+            sum += i;
+            i++;
+            System.out.println("I <3 JAVA");
+        }
+        while (i <= 5);
+
+
+
+    }
+}
