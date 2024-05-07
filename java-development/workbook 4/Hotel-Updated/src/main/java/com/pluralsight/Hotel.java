ce0bb5772475a5e86e82f8bff32155e345c93049
BryaWoods <bryannewoods@gmail.com>
2024-05-07 14:52:48 -0400

    updated hotel class

--- java-development/workbook 4/Hotel-Updated/src/main/java/com/pluralsight/Hotel.java
+++ java-development/workbook 4/Hotel-Updated/src/main/java/com/pluralsight/Hotel.java
@@ -5,19 +5,23 @@ public class Hotel {
     private int numberOfSuites;
     private int numberOfRooms;
     private int bookedRooms;
+    private int bookedSuites;
 
     public Hotel(String name, int numberOfSuites, int numberOfRooms) {
         this.name = name;
-        this.numberOfSuites = 0;
-        this.numberOfRooms = 0;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedRooms = 0;
 
     }
 
-    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedRooms) {
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedRooms, int bookedSuites) {
         this.name = name;
         this.numberOfSuites = numberOfSuites;
         this.numberOfRooms = numberOfRooms;
         this.bookedRooms = bookedRooms;
+        this.bookedSuites = bookedSuites;
     }
 
     public String getName() {
@@ -37,22 +41,30 @@ public class Hotel {
     }
 
     public boolean bookRoom(int numberOfRooms, boolean isSuite) {
-        int totalAvailableRooms = numberOfRooms + (isSuite ? numberOfSuites : numberOfRooms);
-        int availableRooms = totalAvailableRooms - bookedRooms;
-        if (availableRooms >= numberOfRooms && numberOfRooms > 0) {
-            bookedRooms += numberOfRooms;
-            System.out.println("Rooms booked successfully.");
-            return true;
+        if (isSuite) {
+            if (bookedSuites + numberOfRooms <= numberOfRooms) {
+                bookedSuites += numberOfRooms;
+                System.out.println("Rooms booked successfully.");
+                return true;
+            } else {
+                return false;
+            }
         } else {
-            System.out.println("Insufficient rooms available for booking.");
-            return false;
+            if (bookedRooms + numberOfRooms <= numberOfRooms) {
+                bookedRooms += numberOfRooms;
+                System.out.println("Rooms booked successfully.");
+                return true;
+            } else {
+                System.out.println("Insufficient rooms available for booking.");
+                return false;
+            }
 
         }
-
     }
 
+
     public int getAvailableSuites() {
-        return numberOfSuites - bookedRooms;
+        return numberOfSuites - bookedSuites;
     }
 
     public int getAvailableRooms() {
